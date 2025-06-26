0d939849e46de79bba5607f65d5b5959b4977ef3
aycabayraktar <68955010+aycabayraktar@users.noreply.github.com>
2025-06-26 15:00:09 +0300

    prepare_data.py
    added prints

--- prepare_data.py
+++ prepare_data.py
@@ -15,8 +15,10 @@ arg_parts = []
 for v in data_cfg_dict:
     if data_cfg_dict[v].USE_AUGMENT:
         arg_parts.append(f'python utils/aug_data.py --version {v}')
+        print(f"checking data config aug records {data_cfg_dict[v]}")
     else:
         arg_parts.append(f'python utils/data.py --version {v}')
+        print(f"checking data config records {data_cfg_dict[v]}")
 pool = mp.Pool(processes=6)
 pool.map(os.system, arg_parts)
 pool.close()
